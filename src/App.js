4443f78d15fdd1284e93ad2a2402b8fc78f5a089
Madeline Guman <mads890@yahoo.com>
2020-04-22 14:17:22 -0400

    changed app function to a class component, fixed indexjs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import List from './List';
 import './App.css';
 
-function App(props) {
-  return (
-    <main className='App'>
-      <header className='App-header'>
-        <h1>Trelloyes!</h1>
-      </header>
-      <div className='App-list'>
-        <List>
-          {props.store.lists}
-        </List>
-      </div>
+class App extends Component {
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    }
+  };
+
+  render() {
+    const { store } = this.props
+    const customList = store.lists.map(list => (
+      <List 
+        key={list.id} 
+        header={list.header} 
+        cards={list.cardIds.map(id => store.allCards[id])}
+      />
+    ))
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {customList}
+        </div>
     </main>
-  );
+    );
+  }
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -4,19 +4,27 @@ import './List.css';
 
 
 
-function List(props) {
+export default function List(props) {
     return (
         <section className='List'>
             <header className='List-header'>
                 <h2>{props.header}</h2>
             </header>
             <div className='List-cards'>
-                <Card>
-                    {props.cards}
-                </Card>
+                {props.cards.map((card) =>
+                    <Card
+                        key={card.id}
+                        title={card.title}
+                        content={card.content}
+                    />
+                )}
+                <button
+                    type='button'
+                    className='List-add-button'
+                >
+                    + Add Random Card
+                </button>
             </div>
         </section>
-    );
+    )
 }
-
-export default List;
